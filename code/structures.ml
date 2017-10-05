b2a7e2a0df0c58fd4776d5273c124c56be59c0ed
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-10-05 10:08:21 +0200

    wip

--- code/structures.ml
+++ code/structures.ml
@@ -1,23 +1,24 @@
 type value = Float of float | Int of int | Bool of bool | String of string;;
-type valueParameter =  Value of value;;
+type valueParameter = value;;
+type audioParameter = float array array;;
 type parameter = ValueParam of valueParameter;;
 type node = { name: string; param: parameter option; children: node list; };;
 
-let test_p = ValueParam (Value (Float 3.4) ) ;; 
+let test_p = ValueParam (Float 3.4) ;;
 let test_n = { name = "foo"; param = Some test_p; children = [] } ;;
- 
-let pull p = match p with 
+
+let pull p = match p with
   | ValueParam v -> v ;;
-let push p nv = match p with 
-  | ValueParam (Value v) -> ValueParam (Value nv) ;;
+let push p nv = match p with
+  | ValueParam v -> ValueParam nv ;;
 
 type binop = And | Or | Xor;;
 type comparator = Greater | GreaterEq | Lower | LowerEq | Equal | Different ;;
 type atomElement = AtomParam of parameter | AtomValue of value ;;
-type atom = atomElement * atomElement * comparator 
+type atom = atomElement * atomElement * comparator
 and negation = expression
 and composition = expression * expression * binop
-and impulse = parameter * bool 
+and impulse = parameter * bool
 and expression = Atom of atom | Negation of negation | Composition of composition | Impulse of impulse ;;
 
 let test_atom = Atom ((AtomValue (Float 3.4)), (AtomValue (Float 4.5)), Greater);;
@@ -25,13 +26,35 @@ let test_comp = Composition (test_atom, test_atom, Or);;
 
 let evaluate expr = true ;;
 let update expr = expr ;;
- 
- 
+
+
 type status = Waiting | Pending | Happened | Disposed;;
-type process = { };;
-type interval = { minDuration:int; maxDuration : int option; nominalDuration : int; itvStatus: status; processes: process list };;
+type message = { messParam: parameter ; messValue: value };;
+type state = message list;;
 
-let condition = expression * interval list * interval list * status;
+(* ports *)
+type cable = int ;;
+type valuePort = valueParameter * cable list ;;
+type audioPort = audioParameter * cable list ;;
 
+(* some processes *)
+type curve = (float * float) list ;;
+type automation = valuePort * curve;;
+type mapping = valuePort * valuePort * curve;;
+type sound = audioPort * float array array;;
 
-let state proc t = proc 
+type process = Automation of automation | Mapping of mapping | Sound of sound;;
+type interval = {
+    minDuration:int;
+    maxDuration : int option;
+    nominalDuration : int;
+    itvStatus: status;
+    processes: process list
+};;
+
+type condition = expression * (interval list) * (interval list) * status;;
+type trigger = expression * condition list;;
+
+(*let state proc t = proc*)
+let add_process interval proc = match interval with
+  | (a,b,c,d,e) -> (a,b,c,d,proc::e)
